cf7d2a37673d742f258ce6f7e529c0f68caff575
Yuri Ramos <yuri.ramos@libercapital.com.br>
2024-05-16 20:09:03 -0300

    some updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
---
-
 local keymap = vim.keymap
 
 -- Do things without affecting the registers
@@ -19,15 +14,7 @@ keymap.set("n", "<Leader>D", '"_D')
 keymap.set("v", "<Leader>d", '"_d')
 keymap.set("v", "<Leader>D", '"_D')
 
-vim.keymap.set("i", "jk", "<ESC>", { silent = true })
-
 local opts = { noremap = true, silent = true }
-
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
-
--- Delete a word backwards
-keymap.set("n", "dw", 'vb"_d')
 
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
@@ -43,51 +30,73 @@ keymap.set("n", "<C-w><right>", "<C-w>>")
 keymap.set("n", "<C-w><up>", "<C-w>+")
 keymap.set("n", "<C-w><down>", "<C-w>-")
 
+-- dont save to clipboard when delete and change
+keymap.set("n", "d", '"_d')
+keymap.set("n", "c", '"_c')
+
+-- paste from yank register
 keymap.set("n", "p", '"0p')
 keymap.set("n", "P", '"0P')
 
--- New tab
-keymap.set("n", "te", ":tabedit")
-keymap.set("n", "<tab>", ":tabnext<Return>", opts)
-keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 -- Split window
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
+
 -- Move window
 keymap.set("n", "sh", "<C-w>h")
 keymap.set("n", "sk", "<C-w>k")
 keymap.set("n", "sj", "<C-w>j")
 keymap.set("n", "sl", "<C-w>l")
 
--- Disable continuations
-keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
-keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
-
--- Jumplist
-keymap.set("n", "<C-m>", "<C-i>", opts)
-
+-- update and save
 keymap.set({ "n", "i" }, "<C-s>", "<ESC>:up<CR>")
 
-keymap.set("n", "<leader>gn", ":Neogit kind=split<CR>")
-keymap.set("n", "<leader>gN", ":Neogit commit<CR>")
-
+-- open copilot chat
 keymap.set("n", "<leader>cc", ":CopilotChat ")
 keymap.set("n", "<leader>ct", ":CopilotChatToggle<CR>")
 
+-- open diff view
 keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>")
 keymap.set("n", "<leader>gD", ":DiffviewClose<CR>")
 keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame <CR>")
 
+-- set U do redu
 keymap.set("n", "U", "<C-r>")
 
-keymap.set("n", "<leader>Tt", ":ToggleTermToggleAll<CR>")
-
+-- Tmux navigation
 vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { silent = true })
 vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { silent = true })
 vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { silent = true })
 vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { silent = true })
 
+-- Move lines
 keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move line up(v)
 keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- move line down(v)
 
+-- clean highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- set ctrl b to enter in visual block mode
+keymap.set("n", "<C-b>", "<C-v>")
+
+-- add line above and below
+keymap.set("n", "<C-o>", "O<C-c>j")
+keymap.set("n", "<C-m>", "o<C-c>k")
+
+-- double quote a word
+keymap.set("n", "<leader>q", 'viw<ESC>a"<ESC>bi"<ESC>')
+
+-- create space between lines and insert
+keymap.set("i", "<C-o>", "<C-j>")
+
+-- type only one quote when in insert mode
+keymap.set("i", "'", "'")
+keymap.set("i", '"', '"')
+
+-- go left when double quotes are typed
+keymap.set("i", '""', '""<left>')
+keymap.set("i", "''", "''<left>")
+
+-- move left and right in insert mode
+keymap.set("i", "<C-h>", "<left>")
+keymap.set("i", "<C-l>", "<right>")
